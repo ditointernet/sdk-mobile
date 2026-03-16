2728b164382e91e2f128b664d01c84282a842a0a
Roberto Martins <roberto.martinsps3@gmail.com>
2026-03-16 09:36:46 -0300

    Fix/package.swift fixes (#10)
    * chore: Update swift-tools-version to support platfrom .iOS(.v16)
    
    * fix: Fix target path to resolve build errors related to not finding types declared in DitoSDK/Persistence

--- ios/Package.swift
+++ ios/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -16,7 +16,7 @@ let package = Package(
         .target(
             name: "DitoSDK",
             dependencies: [],
-            path: "DitoSDK/Sources",
+            path: "DitoSDK",
             exclude: ["DitoSDK/Info.plist"]
         ),
         .testTarget(
